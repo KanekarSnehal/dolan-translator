fd740d0f8c1a564a9771c8ac2e2e4caaa75a5194
KanekarSnehal <snehalkanekar43@gmail.com>
2021-09-21 23:19:08 +0530

    commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#text-input");
+var outputDiv=document.querySelector("#output");
+
+
+var serverURL="https://api.funtranslations.com/translate/dolan.json"
+
+btnTranslate.addEventListener("click",clickHandler)
+
+
+function clickHandler()
+{
+    var inputText=txtInput.value;
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response =>response.json())
+    .then(json=>{
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
+        
+        
+    .catch(errorHandler)
+    
+};
+
+function getTranslationURL(text)
+{
+    return serverURL+"?"+"text="+text
+}
+
+function errorHandler(error)
+{
+    console.log("error occured",error)
+}
